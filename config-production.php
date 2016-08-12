d9037836574e7d2e4816367aa0f93342a52bff47
Mohamed Said <themohamedsaid@gmail.com>
2016-08-12 10:51:47 +0200

    publishing website

--- config-production.php
+++ config-production.php
@@ -11,7 +11,7 @@ return [
      *
      */
 
-    'base_url' => 'http://themsaid.com/mahberry',
+    'base_url' => 'http://themsaid.com',
 
     /**
      * --------------------------------------------------------------------------
